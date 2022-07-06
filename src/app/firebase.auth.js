7ca4f2a970842d311266637557fcb4477aa66d79
GiovannyOG <Giovannyogodoi@gmail.com>
2022-07-06 10:29:54 -0300

    show username when logged

--- src/app/firebase.auth.js
+++ src/app/firebase.auth.js
@@ -9,35 +9,52 @@ import { getAuth,
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
+// Elements
 const login_buttom = document.getElementById('login_buttom')
 const logout_buttom = document.getElementById('logout_buttom')
-
+const username_container = document.getElementById('username')
+const username_text = document.getElementById('username-text')
+// Hide & Show a element
 const hide_element = (element) => element.style.display = 'none';
 const show_element = (element) => element.style.display = 'inline';
 
+// Change the username
+const change_username = (username) => username_text.innerHTML = username
+
+// Change logout_buttom to login_buttom
 function change_b_toLogin(){
   hide_element(logout_buttom)
   show_element(login_buttom)
 }
 
+// Change login_buttom to logout_buttom
 function change_b_toLogout(){
   hide_element(login_buttom)
   show_element(logout_buttom)
 }
 
+// Update the UI
 function updateUi(user) {
   if (user) {
+    // LogIN
     change_b_toLogout()
+    change_username(user.displayName)
+    show_element(username_container)
   } else {
+    // LogOUT
     change_b_toLogin()
+    hide_element(username_container)
+    change_username('Uknow')
+    hide_element(username_container)
   } 
 }
 
+// Make Firebase Logout
 const logOut = () => {
   signOut(auth)
 }
 
-
+// Make Firebase Login
 const logIn = () => {
   signInWithPopup(auth, provider).then(result => {
     const credentials = GoogleAuthProvider.credential.FromResult(result);
@@ -50,12 +67,12 @@ const logIn = () => {
   });
 }
 
-
+// Change the UI when the login state change
 onAuthStateChanged(auth, (user) => {
-  console.log('logIn state change!')
   updateUi(user)
 });
 
+// Buttoms fucntions
 login_buttom.addEventListener('click', logIn)
 logout_buttom.addEventListener('click', logOut)
 
